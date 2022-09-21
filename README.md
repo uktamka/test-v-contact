1185ffc16b91d1e2d680264d1a8826405906444c
Uktam <uktamka@gmail.com>
2022-09-21 19:45:48 +0500

    feat: implement project

---
+++ README.md
@@ -0,0 +1,39 @@
+# v-contact
+
+web application for managing contacts. Of the main functionscreating, editing, deleting and filtering conflicts.
+
+## Install the dependencies
+
+```bash
+yarn
+# or
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
+```bash
+npm run dev
+```
+
+### Lint the files
+
+```bash
+yarn lint
+# or
+npm run lint
+```
+
+### Format the files
+
+```bash
+yarn format
+# or
+npm run format
+```
+
+### Build the app for production SPA
+
+```bash
+npm build:spa
+```
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> VContact App </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Menu </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue'
+
+const essentialLinks: EssentialLinkProps[] = [
+  {
+    title: 'contacts',
+    caption: '',
+    icon: 'contacts',
+    link: '',
+  },
+  {
+    title: 'Tags',
+    caption: '',
+    icon: 'tag',
+    link: '',
+  },
+]
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
